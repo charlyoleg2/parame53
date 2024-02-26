1086c9eb82d1830370df35d29df75d24c124a8cc
charles <charles.braquet@e.email>
2024-02-26 20:16:29 +0100

    remove boilerplate stuff

--- pkg/desi53/src/pointedCorners/myPartL.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from 'vitest';
-//import * from './myPartL_sub'
-
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
-	});
-});
