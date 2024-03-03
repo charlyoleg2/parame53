6afb469b613a2565c033b212ec82644919cfb076
charles <charles.braquet@e.email>
2024-03-03 07:44:46 +0100

    micro comment fix

--- pkg/desi53b/src/myGroup7/myPartS.test.ts
+++ pkg/desi53b/src/myGroup7/myPartS.test.ts
@@ -1,5 +1,5 @@
 import { describe, it, expect } from 'vitest';
-//import * from './myPartO_sub'
+//import * from './myPartS_sub'
 
 describe('sum test', () => {
 	it('adds 1 + 2 to equal 3', () => {
