1708b9f71dd90211de683f47ae865a1907445ff0
charles <charles.braquet@e.email>
2024-03-27 20:24:59 +0100

    change roof position and use ctrRectangle

--- pkg/desi53b/src/myGroup8/myPartW.ts
+++ pkg/desi53b/src/myGroup8/myPartW.ts
@@ -3,7 +3,7 @@
 
 // step-1 : import from geometrix
 import type {
-	tContour,
+	//tContour,
 	tParamDef,
 	tParamVal,
 	tGeom,
@@ -21,6 +21,7 @@ import {
 	//ShapePoint,
 	contour,
 	//contourCircle,
+	ctrRectangle,
 	figure,
 	//degToRad,
 	radToDeg,
@@ -76,15 +77,8 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 	try {
 		// step-4 : some preparation calculation
 		const roof_angle = Math.atan2(param.H2, param.W1 / 2);
-		const roofV = param.E2 / Math.cos(roof_angle);
-		//const roofH = param.E2 / Math.sin(roof_angle);
-		const roofEndH = Math.cos(Math.PI / 2 - roof_angle) * param.E2;
-		const roofPlusH = param.L1 - roofEndH;
-		const roofPlusV = roofPlusH * Math.tan(roof_angle);
-		const roofPlusLength = Math.sqrt(roofPlusH ** 2 + roofPlusV ** 2);
-		const roofInnerLength = Math.sqrt(param.H2 ** 2 + (param.W1 / 2) ** 2);
-		const roofTopLength = Math.sqrt(roofV ** 2 - param.E2 ** 2);
-		const roofLength = roofTopLength + roofInnerLength + roofPlusLength;
+		const roofLength = (param.W1 / 2 + param.L1) / Math.cos(roof_angle);
+		const roofInnerLength = roofLength - param.E2 * Math.tan(roof_angle);
 		// step-5 : checks on the parameter values
 		if (2 * param.E1 > param.W1) {
 			throw `err295: E1 ${param.E1} is too large compare to W1 ${param.W1}`;
@@ -92,26 +86,15 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 		if (param.E1 > param.T1) {
 			throw `err296: E1 ${param.E1} is too large compare to T1 ${param.T1}`;
 		}
-		if (roofPlusH < 0) {
-			throw `err297: L1 ${param.L1} is too small compare to roof_angle ${ffix(radToDeg(roof_angle))} and E2 ${param.E2}`;
-		}
 		// step-6 : any logs
 		rGeome.logstr += `myPartW: height ${ffix(param.H1 + param.H2)} m\n`;
 		rGeome.logstr += `myPartW: roof-angle ${ffix(radToDeg(roof_angle))} degree\n`;
 		rGeome.logstr += `myPartW: one-side roof-length ${ffix(roofLength)} m\n`;
 		// step-7 : drawing of the figures
-		const ctrRect = function (ox: number, oy: number, lx: number, ly: number): tContour {
-			const rCtr = contour(ox, oy)
-				.addSegStrokeR(lx, 0)
-				.addSegStrokeR(0, ly)
-				.addSegStrokeR(-lx, 0)
-				.closeSegStroke();
-			return rCtr;
-		};
 		// figTop
-		figTop.addMain(ctrRect(-param.W1 / 2, 0, param.W1, param.T1));
+		figTop.addMain(ctrRectangle(-param.W1 / 2, 0, param.W1, param.T1));
 		figTop.addMain(
-			ctrRect(
+			ctrRectangle(
 				-param.W1 / 2 + param.E1,
 				param.E1,
 				param.W1 - 2 * param.E1,
@@ -119,24 +102,29 @@ function pGeom(t: number, param: tParamVal, suffix = ''): tGeom {
 			)
 		);
 		figTop.addSecond(
-			ctrRect(
+			ctrRectangle(
 				-param.W1 / 2 - param.L1,
 				-param.L2,
 				param.W1 / 2 + param.L1,
 				param.T1 + 2 * param.L2
 			)
 		);
-		figTop.addSecond(ctrRect(0, -param.L2, param.W1 / 2 + param.L1, param.T1 + 2 * param.L2));
+		figTop.addSecond(
+			ctrRectangle(0, -param.L2, param.W1 / 2 + param.L1, param.T1 + 2 * param.L2)
+		);
 		// figFace
-		const ctrRoof = contour(-param.W1 / 2 - roofPlusH, param.H1 - roofPlusV)
-			.addSegStrokeA(0, param.H1 + param.H2)
-			.addSegStrokeA(param.W1 / 2 + roofPlusH, param.H1 - roofPlusV)
-			.addSegStrokeRP(Math.PI / 2 - roof_angle, param.E2)
-			.addSegStrokeA(0, param.H1 + param.H2 + roofV)
+		const ctrRoof = contour(0, param.H1 + param.H2)
 			.addSegStrokeRP(Math.PI + roof_angle, roofLength)
+			.addSegStrokeRP(-Math.PI / 2 + roof_angle, param.E2)
+			.addSegStrokeRP(roof_angle, roofInnerLength)
+			.addSegStrokeRP(-roof_angle, roofInnerLength)
+			.addSegStrokeRP(Math.PI / 2 - roof_angle, param.E2)
 			.closeSegStroke();
 		figFace.addMain(ctrRoof);
-		figFace.addSecond(ctrRect(-param.W1 / 2, 0, param.W1, param.H1));
+		figFace.addSecond(ctrRectangle(-param.W1 / 2, 0, param.W1, param.H1));
+		figFace.addSecond(
+			ctrRectangle(-param.W1 / 2 + param.E1, 0, param.W1 - 2 * param.E1, param.H1)
+		);
 		// final figure list
 		rGeome.fig = {
 			faceTop: figTop,
